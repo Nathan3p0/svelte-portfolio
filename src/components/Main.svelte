743d6a2d961c446d94d28e9dfefbd50e88deb484
Nate Szelag <nszelag@getmatterhorn.com>
2023-10-15 23:33:15 -0400

    Add contact section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -46,7 +46,7 @@
 			<a
 				href="https://www.linkedin.com/in/nathan-szelag"
 				target="_blank"
-				class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+				class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
 			>
 				<div
 					class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
