d7ce6b3ab143a66c25ab6ead1b1045723aa9e61a
Nate Szelag <nszelag@getmatterhorn.com>
2023-10-15 22:27:49 -0400

    Add projects section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,4 +1,20 @@
 <script>
+	import Step from './Step.svelte';
+
+	let steps = [
+		{
+			name: 'Smoljames Store',
+			icon: 'fa-solid fa-cart-shopping'
+		},
+		{
+			name: 'Ultimate Todos',
+			icon: 'fa-solid fa-list-check'
+		},
+		{
+			name: 'Pokedex',
+			icon: 'fa-solid fa-diagram-project'
+		}
+	];
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -12,14 +28,61 @@
 				My <span class="text-violet-400">favorite tech</span> includes Javascript, React, Svelte, Node, and MongoDB.
 			</p>
 			<a
-				href=""
-				class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+				href="https://www.linkedin.com/in/nathan-szelag"
+				target="_blank"
+				class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
 			>
 				<div
 					class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
 				/>
 				<h4 class="relative z-9">Get in touch &rarr;</h4>
 			</a>
+		</div>
+		<div class="relative shadow-2xl grid place-items-center">
+			<img
+				src={'images/profile.png'}
+				alt="Emoji of Nate Szelag in a coffee pot holding a cup of coffee"
+				class="object-cover z-[2] max-h-[70vh]"
+			/>
+		</div>
+	</section>
+	<section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+		<div class="flex flex-col gap-2 text-center">
+			<h6 class="text-lg sm:text-xl md:text-2xl">A few of my creative endeavors.</h6>
+			<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+				Curious to <span class="poppins text-violet-400">see</span> my work?
+			</h3>
+		</div>
+		<a
+			href="https://www.youtube.com"
+			target="_blank"
+			class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+		>
+			<i class="fa-regular fa-circle-play" />
+			<p>Watch the video</p>
+		</a>
+		<div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+			<Step step={steps[0]}
+				><p>
+					Smoljames Store is a merchandising store created with <strong class="text-violet-400"
+						>Next.js Commerce.js Stripe & Node.js + Express.js!</strong
+					> Commerce.js is a product CMS and Stripe is used for all transaction handling.
+				</p></Step
+			>
+			<Step step={steps[1]}
+				><p>
+					Smoljames Store is a merchandising store created with <strong class="text-violet-400"
+						>Next.js Commerce.js Stripe & Node.js + Express.js!</strong
+					> Commerce.js is a product CMS and Stripe is used for all transaction handling.
+				</p></Step
+			>
+			<Step step={steps[2]}
+				><p>
+					Smoljames Store is a merchandising store created with <strong class="text-violet-400"
+						>Next.js Commerce.js Stripe & Node.js + Express.js!</strong
+					> Commerce.js is a product CMS and Stripe is used for all transaction handling.
+				</p></Step
+			>
 		</div>
 	</section>
 </main>
